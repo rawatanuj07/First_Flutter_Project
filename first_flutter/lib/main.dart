64797861d7a8ef3fddd15911f6e858c5e25e56fb
Anuj Rawat <rawatanuj07@gmail.com>
2023-01-14 20:47:13 +0530

    StateLess Widget

--- first_flutter/lib/main.dart
+++ first_flutter/lib/main.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("hello warriors"),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text("The Warrior!"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text("click"),
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("hello warriors"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          "The Warrior!",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+          ),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("click "),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
