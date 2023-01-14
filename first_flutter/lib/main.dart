569829c4f7dd388d4e7eb75bf9c6fbf29a81198e
Anuj Rawat <rawatanuj07@gmail.com>
2023-01-14 17:50:05 +0530

    added buttons and other features

--- first_flutter/lib/main.dart
+++ first_flutter/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+void main() => runApp(MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-      title: const Text("hello warriors"),
-    )),
-  ));
-}
+          title: const Text("hello warriors"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("The Warrior!"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("click"),
+        ),
+      ),
+    ));
